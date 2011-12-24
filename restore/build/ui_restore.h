a902a30ac2f37ddeb7c739bb9362f3b60e87f590
Micah Boswell <mtboswell@gmail.com>
2011-12-23 23:17:23 -0500

    Updated restore UI

--- restore/build/ui_restore.h
+++ restore/build/ui_restore.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'restore.ui'
 **
-** Created: Fri Dec 23 14:50:10 2011
+** Created: Fri Dec 23 22:42:48 2011
 **      by: Qt User Interface Compiler version 4.8.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -16,7 +16,9 @@
 #include <QtGui/QButtonGroup>
 #include <QtGui/QHBoxLayout>
 #include <QtGui/QHeaderView>
+#include <QtGui/QLineEdit>
 #include <QtGui/QPushButton>
+#include <QtGui/QToolButton>
 #include <QtGui/QTreeView>
 #include <QtGui/QTreeWidget>
 #include <QtGui/QVBoxLayout>
@@ -28,11 +30,16 @@ class Ui_RestoreWidget
 {
 public:
     QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout_2;
+    QHBoxLayout *horizontalLayout_3;
+    QToolButton *upButton;
+    QLineEdit *fileRootPath;
+    QPushButton *goButton;
     QTreeView *treeView;
     QVBoxLayout *verticalLayout;
     QTreeWidget *treeWidget;
     QHBoxLayout *horizontalLayout_2;
-    QPushButton *pushButton;
+    QPushButton *restoreButton;
 
     void setupUi(QWidget *RestoreWidget)
     {
@@ -41,10 +48,37 @@ public:
         RestoreWidget->resize(662, 394);
         horizontalLayout = new QHBoxLayout(RestoreWidget);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, -1, -1);
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setContentsMargins(-1, 0, 0, -1);
+        upButton = new QToolButton(RestoreWidget);
+        upButton->setObjectName(QString::fromUtf8("upButton"));
+
+        horizontalLayout_3->addWidget(upButton);
+
+        fileRootPath = new QLineEdit(RestoreWidget);
+        fileRootPath->setObjectName(QString::fromUtf8("fileRootPath"));
+
+        horizontalLayout_3->addWidget(fileRootPath);
+
+        goButton = new QPushButton(RestoreWidget);
+        goButton->setObjectName(QString::fromUtf8("goButton"));
+
+        horizontalLayout_3->addWidget(goButton);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_3);
+
         treeView = new QTreeView(RestoreWidget);
         treeView->setObjectName(QString::fromUtf8("treeView"));
 
-        horizontalLayout->addWidget(treeView);
+        verticalLayout_2->addWidget(treeView);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
 
         verticalLayout = new QVBoxLayout();
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
@@ -60,10 +94,10 @@ public:
         horizontalLayout_2 = new QHBoxLayout();
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
         horizontalLayout_2->setContentsMargins(-1, 0, -1, -1);
-        pushButton = new QPushButton(RestoreWidget);
-        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        restoreButton = new QPushButton(RestoreWidget);
+        restoreButton->setObjectName(QString::fromUtf8("restoreButton"));
 
-        horizontalLayout_2->addWidget(pushButton);
+        horizontalLayout_2->addWidget(restoreButton);
 
 
         verticalLayout->addLayout(horizontalLayout_2);
@@ -73,6 +107,7 @@ public:
 
 
         retranslateUi(RestoreWidget);
+        QObject::connect(fileRootPath, SIGNAL(editingFinished()), goButton, SLOT(click()));
 
         QMetaObject::connectSlotsByName(RestoreWidget);
     } // setupUi
@@ -80,7 +115,9 @@ public:
     void retranslateUi(QWidget *RestoreWidget)
     {
         RestoreWidget->setWindowTitle(QApplication::translate("RestoreWidget", "Restore", 0, QApplication::UnicodeUTF8));
-        pushButton->setText(QApplication::translate("RestoreWidget", "Restore", 0, QApplication::UnicodeUTF8));
+        upButton->setText(QApplication::translate("RestoreWidget", "UP", 0, QApplication::UnicodeUTF8));
+        goButton->setText(QApplication::translate("RestoreWidget", "Go", 0, QApplication::UnicodeUTF8));
+        restoreButton->setText(QApplication::translate("RestoreWidget", "Restore", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
 };
