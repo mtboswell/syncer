4e95a1e2c2750296e43f073078857f03df3cd821
Micah Boswell <mtboswell@gmail.com>
2011-12-29 02:47:14 -0500

    Autosync commit

--- undelete/main.cpp
+++ undelete/main.cpp
@@ -5,9 +5,11 @@ int main(int argc, char** argv){
 	QApplication app(argc,argv);
 	QStringList args = app.arguments();
 	args.removeFirst();
+	
+	if(args.size() == 0) return 1;
 
 	Undelete undelete(args[0]);
 	undelete.show();
 
-	app.exec();
+	return app.exec();
 }
