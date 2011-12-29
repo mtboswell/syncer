6bc208816e72646ceaa1e589c7bcaec8eb99b026
Micah Boswell <mtboswell@gmail.com>
2011-12-28 23:37:08 -0500

    Autosync commit

--- syncer-gui/qrc_icons.cpp
+++ syncer-gui/qrc_icons.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Wed Dec 28 21:30:27 2011
+** Created: Wed Dec 28 23:35:48 2011
 **      by: The Resource Compiler for Qt version 4.8.0
 **
 ** WARNING! All changes made in this file will be lost!
