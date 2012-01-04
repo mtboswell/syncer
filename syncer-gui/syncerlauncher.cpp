ab896be9ef1f910d0bfb81316cdcb40c6a0670e7
Micah Boswell <mtboswell@gmail.com>
2012-01-03 21:50:53 -0500

    Autosync commit

--- syncer-gui/syncerlauncher.cpp
+++ syncer-gui/syncerlauncher.cpp
@@ -12,10 +12,7 @@ SyncerLauncher::SyncerLauncher(QObject *parent) :
 	connect(procOutMapper, SIGNAL(mapped(const QString &)), this, SLOT(readProcOut(const QString &)));
 
 	//syncerPath = "../../../syncer/build/release/syncer.exe";
-<<<<<<< HEAD
-=======
 	// todo: use app->appPath(), etc.
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
 	if(settings->contains("syncerPath")){
 		syncerPath = settings->value("syncerPath").toString();
 	}else{
@@ -28,19 +25,12 @@ SyncerLauncher::SyncerLauncher(QObject *parent) :
 
 	syncMenu = new QMenu;
 
-<<<<<<< HEAD
-	QAction* addAction = syncMenu->addAction("Add Directory");
-	QAction* quitAction = syncMenu->addAction("Quit");
-	syncMenu->addSeparator();
-
-=======
 	QAction* initAction = syncMenu->addAction("Add Directory From Server");
 	QAction* addAction = syncMenu->addAction("Re-Add Forgotten Directory");
 	QAction* quitAction = syncMenu->addAction("Quit");
 	syncMenu->addSeparator();
 
 	connect(initAction, SIGNAL(triggered()), this, SLOT(initPath()));
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
 	connect(addAction, SIGNAL(triggered()), this, SLOT(addPath()));
 	connect(quitAction, SIGNAL(triggered()), this, SLOT(quitAll()));
 
@@ -51,8 +41,6 @@ SyncerLauncher::SyncerLauncher(QObject *parent) :
 
 	trayIcon->setContextMenu(syncMenu);
 
-<<<<<<< HEAD
-=======
 	refreshMenu();
 
 	trayIcon->show();
@@ -65,17 +53,11 @@ void SyncerLauncher::refreshMenu(){
 		syncMenu->removeAction(findMenuItem(syncMenu, path));
 	}
 
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
 	QStringList dirs = settings->value("syncDirs").toStringList();
 
 	foreach(QString dir, dirs){
 		addPath(dir);
 	}
-<<<<<<< HEAD
-
-	trayIcon->show();
-
-=======
 }
 
 void SyncerLauncher::initPath(){
@@ -83,7 +65,6 @@ void SyncerLauncher::initPath(){
 	connect(init, SIGNAL(finished(int)), this, SLOT(refreshMenu()));
 	connect(init, SIGNAL(finished(int)), init, SLOT(deleteLater()));
 	init->show();
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
 }
 
 void SyncerLauncher::addPath(){
@@ -109,11 +90,7 @@ void SyncerLauncher::addPath(QString dir){
 	connect(removeAction, SIGNAL(triggered()), menuMapper, SLOT(map()));
 	menuMapper->setMapping(removeAction, "Remove:" + dir);
 
-<<<<<<< HEAD
-	QAction* restoreAction = dirMenus[dir]->addAction("Restore Files");
-=======
 	QAction* restoreAction = dirMenus[dir]->addAction("Restore Files from earlier versions");
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
 	connect(restoreAction, SIGNAL(triggered()), menuMapper, SLOT(map()));
 	menuMapper->setMapping(restoreAction, "Restore:" + dir);
 
@@ -121,13 +98,10 @@ void SyncerLauncher::addPath(QString dir){
 	connect(undeleteAction, SIGNAL(triggered()), menuMapper, SLOT(map()));
 	menuMapper->setMapping(undeleteAction, "Undelete:" + dir);
 
-<<<<<<< HEAD
-=======
 	QAction* shredAction = dirMenus[dir]->addAction("Permenantly Delete Files");
 	connect(shredAction, SIGNAL(triggered()), menuMapper, SLOT(map()));
 	menuMapper->setMapping(shredAction, "Shred:" + dir);
 
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
 	start(dir);
 }
 
@@ -137,10 +111,7 @@ void SyncerLauncher::doAction(QString action){
 	else if(action.startsWith("Remove:")) remove(action.right(action.length()-7));
 	else if(action.startsWith("Restore:")) restore(action.right(action.length()-8));
 	else if(action.startsWith("Undelete:")) undelete(action.right(action.length()-9));
-<<<<<<< HEAD
-=======
 	else if(action.startsWith("Shred:")) shred(action.right(action.length()-6));
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
 }
 
 void SyncerLauncher::start(QString path){
@@ -203,8 +174,6 @@ void SyncerLauncher::undelete(QString path){
 	if(!undeleteProc->waitForStarted()) qWarning("Error: undelete did not start");
 }
 
-<<<<<<< HEAD
-=======
 void SyncerLauncher::shred(QString path){
 	QProcess* shredProc = new QProcess();
 	QStringList args;
@@ -212,9 +181,6 @@ void SyncerLauncher::shred(QString path){
 	shredProc->start("syncer-shred", args);
 	if(!shredProc->waitForStarted()) qWarning("Error: shred did not start");
 }
->>>>>>> 56d4365c719c90d1f0874862f6f9c3ff69af3581
-
-
 
 
 QAction* SyncerLauncher::findMenuItem(QMenu* menu, QString item){
