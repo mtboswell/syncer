d81f594ae881550d528c4b449cfb70ca37c274b5
Micah Boswell <mtboswell@gmail.com>
2012-01-11 18:19:41 -0500

    Autosync commit

--- syncer-gui/init.cpp
+++ syncer-gui/init.cpp
@@ -364,7 +364,6 @@ bool Init::gitClone(QString localFolder, QString username, QString host, int por
 
 	QProcess* shProc = new QProcess();
 	shProc->setProcessChannelMode(QProcess::MergedChannels);
-	shProc->setWorkingDirectory(localFolder);
 
 	shProc->start("sh", QStringList() << "--login" << "-i");
 	if(!shProc->waitForStarted()){
@@ -377,7 +376,10 @@ bool Init::gitClone(QString localFolder, QString username, QString host, int por
 //	QString shOut = shProc->readAll();
 //	QMessageBox::information(this, "Sh:", shOut);
 
-	shProc->write(QString("git clone ssh://" + username + "@" + host + ":" + QString::number(port)
+    shProc->write("export PATH=$PATH:$PWD\n");
+
+    shProc->setWorkingDirectory(localFolder);
+    shProc->write(QString("git clone ssh://" + username + "@" + host + ":" + QString::number(port)
 				+ "/~/" + shareName).toLatin1());
 	shProc->write("\n");
 
